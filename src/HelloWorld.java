0718da2f79542f4aa9fc72b403b7600a7dbb5d60
Leena <leena4talk@gmail.com>
2022-05-06 12:58:54 +0530

    My modified file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,9 @@ public class HelloWorld {
         System.out.println("Welcome back to Java and GIT! ");
         System.out.print("I will print on the same line.");
         System.out.println("GIT integration! ");
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
     }
 }
