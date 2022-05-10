dff5a3172bcd0f74c1a6b579d12d7993d2bbc084
Leena-Auxilia <104761000+Leena-Auxilia@users.noreply.github.com>
2022-05-10 20:02:23 +0530

    fetch testing

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome back to Java and GIT! ");
         System.out.print("I will print on the testing same line.");
-        System.out.println("GIT integration! ");
+        System.out.println("GIT integration for pull! ");
         char myVar1 = 65, myVar2 = 66, myVar3 = 67;
         System.out.println(myVar1);
         System.out.println(myVar2);
